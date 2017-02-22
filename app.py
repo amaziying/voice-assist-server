9375d70c1c42a63a5fbd11dfa5732c22b5768825
Ziying Zhang <ziyingzhanginc@gmail.com>
2017-02-22 23:13:03 +0800

    Fix indentation

--- app.py
+++ app.py
@@ -11,22 +11,18 @@ def hello_world():
 
 @app.route('/request', methods=['POST'])
 def request_handler():
-	data = request.get_json()
-	text = data['transcription']
+    data = request.get_json()
+    text = data['transcription']
 
-	response = alchemy_language.keywords(text=text)
-	keywords = map(lambda x: x['text'], response['keywords'])
+    response = alchemy_language.keywords(text=text)
+    keywords = map(lambda x: x['text'], response['keywords'])
 
-	if len(keywords) > 0:
-		sentiment = alchemy_language.targeted_sentiment(text=text,
-				                                        targets=keywords,
-				                                        language='english')
-		result = sentiment['results']
-	else:
-		result = []
+    if len(keywords) > 0:
+        sentiment = alchemy_language.targeted_sentiment(text=text,
+                                                        targets=keywords,
+                                                        language='english')
+        result = sentiment['results']
+    else:
+        result = []
 
-	return jsonify({'result': result})
-
-
-
-
+    return jsonify({'result': result})
