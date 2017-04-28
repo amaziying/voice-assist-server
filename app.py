9e3302bc1a280e99fab44f7f2e3192b39367ebd8
Ziying Zhang <ziyingzhanginc@gmail.com>
2017-04-29 00:44:07 +0800

    APIs and new updates

--- app.py
+++ app.py
@@ -1,25 +1,66 @@
 import json
+
 from flask import Flask, request, jsonify
 from watson_developer_cloud import NaturalLanguageUnderstandingV1
 import watson_developer_cloud.natural_language_understanding.features.v1 as features
 
+from triage_manager import enqueue, get_active_requests, get_closed_requests, pickup_request, close_request
+
 app = Flask('Voice assist app')
+
+# Connect to IBM Watson
 natural_language_understanding = NaturalLanguageUnderstandingV1(
     version='2017-02-17',
     username='632402de-fa93-4f20-ab17-0b8b8ccad28e',
     password='VQdbAVLmc1sI')
 
+watson_cache = {}
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/request', methods=['POST'])
-def request_handler():
+@app.route('/api/requests', methods=['POST'])
+def open_request():
     data = request.get_json()
     text = data['transcription']
+    patient_id = data['patient_id']
 
-    result = natural_language_understanding.analyze(
-        text=text,
-        features=[features.Keywords(), features.Sentiment()])
+    if text not in watson_cache:
+        watson_cache[text] = natural_language_understanding.analyze(
+            text=text,
+            features=[features.Keywords(), features.Sentiment()])
+
+    enqueue(patient_id, text, watson_cache[text])
+    return jsonify({'result': watson_cache[text]})
+
+@app.route('/api/requests', methods=['GET'])
+def list_requests():
+    finder = request.args.get('finder')
+    result = []
+
+    if finder == 'active':
+        result = get_active_requests()
+    elif finder == 'closed':
+        result = get_closed_requests()
 
     return jsonify({'result': result})
+
+
+@app.route('/api/pickup_request', methods=['POST'])
+def pickup():
+    data = request.get_json()
+    request_id = data['request_id']
+
+    pickup_request(request_id)
+
+    return jsonify({})
+
+@app.route('/api/close_request', methods=['POST'])
+def close():
+    data = request.get_json()
+    request_id = data['request_id']
+
+    close_request(request_id)
+
+    return jsonify({})
---
+++ triage_manager.py
@@ -0,0 +1,92 @@
+import os
+import urlparse
+
+import psycopg2
+
+# TODO: Move constants to DB/cache
+high_p = ['pain', 'hurt', 'hurts', 'medicine', 'nausea', 'nauseous', 'dizzy', 'die', 'dying', 'death']
+med_p = ['washroom', 'bathroom', 'toilet', 'uncomfortable']
+low_p = ['water', 'food', 'ice', 'blanket', 'pillow', 'bed', 'adjust']
+
+# Connect to PostgreSQL DB
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
+
+def get_keyword_score(score, keyword):
+    weight = 1
+    text = keyword['text'].lower()
+
+    if text in high_p:
+        weight = 3
+    elif text in med_p:
+        weight = 2
+
+    return score + weight*keyword['relevance']
+
+def get_priority_score(sentiment, keywords):
+    keyword_score = reduce(get_keyword_score, keywords, 1)
+    sentiment_adjustment = 1 + abs(sentiment['score']) if sentiment['label'] == 'negative' else 1
+    
+    return keyword_score*sentiment_adjustment
+
+def enqueue(patient_id, text, result):
+    score = get_priority_score(result['sentiment']['document'], result['keywords'])
+    try:
+        cur = conn.cursor()
+        cur.execute(
+            """INSERT INTO Requests (patient_id, transcription, status, priority_score) VALUES (%s, %s, %s, %s)""",
+            (patient_id, text, 'OPEN', score))
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+def get_active_requests():
+    result = []
+
+    try:
+        cur = conn.cursor()
+        cur.execute("""SELECT * FROM Requests WHERE status<>%s ORDER BY priority_score DESC """, ('CLOSED',))
+        result = cur.fetchall()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+    return result
+
+def get_closed_requests():
+    result = []
+
+    try:
+        cur = conn.cursor()
+        cur.execute("""SELECT * FROM Requests WHERE status=%s ORDER BY priority_score DESC """, ('CLOSED',))
+        result = cur.fetchall()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+    return result
+
+def pickup_request(id):
+    try:
+        cur = conn.cursor()
+        cur.execute(
+            """UPDATE Requests SET status=%s, ts_pickup=CURRENT_TIMESTAMP WHERE id=%s """,
+            ('INPROGRESS', id))
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+def close_request(id):
+    try:
+        cur = conn.cursor()
+        cur.execute(
+            """UPDATE Requests SET status=%s, ts_close=CURRENT_TIMESTAMP WHERE id=%s """,
+            ('CLOSED', id))
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
